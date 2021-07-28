795fa2157e534b6c0d5ae1ec06fa94d8145cc63d
gustavo guimaraes <gustavo.26.guimaraes@gmail.com>
2021-07-27 22:32:58 -0300

    first commit

---
+++ 5 - Operadores/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("OPERADORES")
+	// aritméticos +, -, *, /, %
+	fmt.Println("Aritméticos:")
+	soma := 10 + 1
+	fmt.Println("soma")
+	fmt.Println(soma)
+
+	subtracao := 10 - 1
+	fmt.Println("subtracao")
+	fmt.Println(subtracao)
+
+	divisao := 10 / 2
+	fmt.Println("divisao")
+	fmt.Println(divisao)
+
+	multiplicacao := 10 * 3
+	fmt.Println("multiplicacao")
+	fmt.Println(multiplicacao)
+
+	modulo := 10 % 3
+	fmt.Println("modulo")
+	fmt.Println(modulo)
+}
